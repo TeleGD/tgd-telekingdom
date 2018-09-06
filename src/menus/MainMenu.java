b81b181445f12aee56757b1c66e5be7d3a8454d1
Amos GEORGE <amos.george0@gmail.com>
2018-09-06 15:08:36 +0200

    Initial commit

---
+++ src/menus/MainMenu.java
@@ -0,0 +1,49 @@
+package menus;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+
+
+public class MainMenu extends Menu{
+
+	public static int ID = -3;	
+	
+	public MainMenu(){
+		super.setTitrePrincipal("INSERER TITRE ICI");
+		super.setTitreSecondaire("SOUS TITRE");
+		
+		super.setItems(telekingdom.World.GAME_NAME,"Scores", "Quitter");
+
+		super.setEnableClignote(false);
+		super.setCouleurClignote(Color.red);
+		super.setTempsClignote(400);
+	}
+	
+	@Override
+	public void onOptionItemFocusedChanged(int position) {
+		time=System.currentTimeMillis();
+	}
+
+	@Override
+	public void onOptionItemSelected(int position) {
+		switch (position) {
+		case 0:
+			telekingdom.World.reset();
+			game.enterState(telekingdom.World.ID, new FadeOutTransition(),
+			new FadeInTransition());
+			break;
+		case 2:
+			System.out.println("exit");
+			System.exit(0);
+			break;
+		}
+	}
+	
+	@Override
+	public int getID() {
+		return ID;
+	}
+
+}
