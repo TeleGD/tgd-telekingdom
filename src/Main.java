5749e44e2e669904009cd6215356249b32720364
Amos GEORGE <amos.george0@gmail.com>
2018-09-13 17:45:03 +0200

    Choix de plein écran

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import org.newdawn.slick.SlickException;
 
 import app.AppContainer;
@@ -7,12 +9,27 @@ public final class Main {
 
 	public static final void main (String [] arguments) throws SlickException {
 		AppGame appGame = new AppGame ("TeleKingdom");
-		AppContainer container = new AppContainer (appGame, 1920, 1080, false);
+		AppContainer container = new AppContainer (appGame, 1280, 720, false);
+		Object[] options = {"Oui",
+        "Non"};
+		int n = JOptionPane.showOptionDialog(null,
+				"Voulez-vous jouer en plein écran ?",
+				"42",
+				JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,     //do not use a custom Icon
+				options,  //the titles of buttons
+				options[0]); //default button title
+		
+		if (n==0) {
+			container = new AppContainer (appGame, 1920, 1080, false);
+			container.setFullscreen(true);
+		} else {
+			container.setFullscreen(false);
+		}
 		container.setTargetFrameRate (60);
-		container.setFullscreen(true);
 		container.setVSync (true);
 		container.setShowFPS (false);
 		container.start ();
 	}
-
 }
