76c5ef559e7281cddbe7557e42cd76322303e411
Tristan LE GODAIS <tristan.le-godais@telecomnancy.eu>
2018-09-26 20:10:21 +0200

    Factorisation de code dans le fichier Request.java

--- src/telekingdom/hud/Request.java
+++ src/telekingdom/hud/Request.java
@@ -9,39 +9,31 @@ import telekingdom.World;
 
 public class Request {
 
-	private static float WINDOW_WIDTH = 1280f;
-	private static float WINDOW_HEIGHT = 720f;
-	private static float BOX_X = 498f;
-	private static float BOX_Y = 160f;
-	private static float BOX_WIDTH = 284f;
-	private static float BOX_HEIGHT = 168f;
-	private static float LINE_HEIGHT = .8f;
+	private static float WINDOW_WIDTH = 1280f; // la largeur de la fenêtre
+	private static float WINDOW_HEIGHT = 720f; // la hauteur de la fenêtre
+	private static float BOX_X = 498f; // l'abscisse de la zone de texte
+	private static float BOX_Y = 160f; // l'ordonnée de la zone de texte
+	private static float BOX_WIDTH = 284f; // la largeur de la zone de texte
+	private static float BOX_HEIGHT = 168f; // la hauteur de la zone de texte
+	private static float LINE_HEIGHT = .8f; // le ratio de la hauteur de ligne sur la hauteur de fonte
 
-	/* Position centrale de la boîte de texte sur l'écran */
-	private float x;
-	private float y;
-
-	/* Tableaux contenant les différentes lignes de texte à afficher et leur taille respective */
-	private String [] lines;
-	private float [] lineWidths;
-	private float lineHeight;
+	private String [] lines; // les lignes de texte
+	private int [] x; // l'abscisse de chaque ligne de texte
+	private int [] y; // l'ordonnée de chaque ligne de texte
 
 	public Request (String description, World world) {
-		/* Décomposition de la chaîne de caractères `description` pour l'afficher sur plusieurs lignes */
-
-		float horizontalZoom = world.getWidth () / Request.WINDOW_WIDTH;
-		float verticalZoom = world.getHeight () / Request.WINDOW_HEIGHT;
-		float width = Request.BOX_WIDTH * horizontalZoom;
-		float height = Request.BOX_HEIGHT * verticalZoom;
-		x = Request.BOX_X * horizontalZoom + width / 2f;
-		y = Request.BOX_Y * verticalZoom + height / 2f;
-
 		description = description.replaceAll ("^\\s+|\\s+$", "");
-
 		if (description.isEmpty ()) {
-			lines = new String [0];
-			lineWidths = new float [0];
+			this.lines = new String [0];
+			this.x = new int [0];
+			this.y = new int [0];
 		} else {
+			float horizontalZoom = world.getWidth () / Request.WINDOW_WIDTH;
+			float verticalZoom = world.getHeight () / Request.WINDOW_HEIGHT;
+			float width = Request.BOX_WIDTH * horizontalZoom;
+			float height = Request.BOX_HEIGHT * verticalZoom;
+			float x = Request.BOX_X * horizontalZoom + width / 2f;
+			float y = Request.BOX_Y * verticalZoom + height / 2f;
 			String [] words = description.split ("\\s+");
 			int length = 1;
 			for (int i = 0, j = 1, l = words.length; j < l; j++) {
@@ -54,24 +46,27 @@ public class Request {
 					length++;
 				}
 			}
-			lines = new String [length];
-			lineWidths = new float [length];
+			this.lines = new String [length];
+			this.x = new int [length];
+			this.y = new int [length];
+			float span = (float) World.Font.getHeight (description) * Request.LINE_HEIGHT;
+			float shift = span * ((float) length - 1f + 1f / Request.LINE_HEIGHT) / 2f;
 			for (int i = 0, j = 0, l = words.length; j < l; j++) {
 				if (words [j] != null) {
-					lineWidths [i] = World.Font.getWidth (lines [i++] = words [j]);
+					this.lines [i] = words [j];
+					this.x [i] = (int) (x - (float) World.Font.getWidth (this.lines [i]) / 2f);
+					this.y [i] = (int) (y - shift + span * (float) i);
+					i++;
 				}
 			}
 		}
-		lineHeight = World.Font.getHeight (description) * Request.LINE_HEIGHT;
 	}
 
 	public void render (GameContainer container, StateBasedGame game, Graphics context) {
 		context.setColor (Color.black);
 		context.setFont (World.Font);
 		for (int i = 0, l = lines.length; i < l; i++) {
-			float x = (float) this.x - this.lineWidths [i] / 2f;
-			float y = (float) this.y - this.lineHeight * (((float) l - 1f + 1f / Request.LINE_HEIGHT) / 2f - (float) i);
-			context.drawString (this.lines [i], (int) x, (int) y);
+			context.drawString (this.lines [i], this.x [i], this.y [i]);
 		}
 	}
 
